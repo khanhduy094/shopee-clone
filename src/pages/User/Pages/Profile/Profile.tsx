a12e9dd6bbfcb1e95a5a31e1377cc85608d98960
Nguyen Khanh Duy <khanhduy090499@gmail.com>
2023-03-23 22:46:30 +0700

    render and validate profile user form

--- src/pages/User/Pages/Profile/Profile.tsx
+++ src/pages/User/Pages/Profile/Profile.tsx
@@ -1,28 +1,116 @@
-import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Fragment, useEffect } from 'react'
+import { Controller, useForm, useFormContext } from 'react-hook-form'
+import { useMutation, useQuery } from 'react-query'
+import userApi from 'src/apis/user.api'
 import Button from 'src/components/Button'
 import Input from 'src/components/Input'
+import InputNumber from 'src/components/InputNumber'
+import { userSchema, UserType } from 'src/utils/rules'
+import DateSelect from '../../components/DateSelect'
+
+function Info() {
+  // const {
+  //   register,
+  //   control,
+  //   formState: { errors }
+  // } = useFormContext<FormData>()
+  return (
+    <Fragment>
+      <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
+        <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Tên</div>
+        <div className='sm:w-[80%] sm:pl-5'>
+          <Input
+            classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+            // register={register}
+            name='name'
+            placeholder='Tên'
+            // errorMessage={errors.name?.message}
+          />
+        </div>
+      </div>
+      <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+        <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Số điện thoại</div>
+        <div className='sm:w-[80%] sm:pl-5'>
+          {/* <Controller
+            control={control}
+            name='phone'
+            render={({ field }) => (
+              <InputNumber
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                placeholder='Số điện thoại'
+                errorMessage={errors.phone?.message}
+                {...field}
+                onChange={field.onChange}
+              />
+            )}
+          /> */}
+        </div>
+      </div>
+    </Fragment>
+  )
+}
+
+type FormData = Pick<UserType, 'name' | 'phone' | 'avatar' | 'date_of_birth' | 'address'>
+const profileSchema = userSchema.pick(['name', 'address', 'phone', 'avatar', 'date_of_birth'])
 
 export default function Profile() {
   const {
     register,
     formState: { errors },
-    control
-  } = useForm()
+    control,
+    handleSubmit,
+    setValue
+  } = useForm<FormData>({
+    defaultValues: {
+      name: '',
+      phone: '',
+      address: '',
+      avatar: '',
+      date_of_birth: new Date(1990, 0, 1)
+    },
+    resolver: yupResolver(profileSchema)
+  })
+  const { data: profileData } = useQuery({
+    queryKey: ['profile'],
+    queryFn: userApi.getProfile
+  })
+  const updateProfileMutation = useMutation({
+    mutationFn: userApi.updateProfile
+  })
+  const profile = profileData?.data.data
+  console.log(profile)
+  useEffect(() => {
+    if (profile) {
+      setValue('name', profile.name)
+      setValue('phone', profile.phone)
+      setValue('address', profile.address)
+      setValue('avatar', profile.avatar)
+      setValue('date_of_birth', profile.date_of_birth ? new Date(profile.date_of_birth) : new Date(1990, 0, 1))
+    }
+  }, [profile, setValue])
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data)
+
+    // await updateProfileMutation.mutateAsync({})
+  })
+
   return (
     <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
       <div className='border-b border-b-gray-200 py-6'>
         <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ Sơ Của Tôi</h1>
         <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
       </div>
-      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start' onSubmit={onSubmit}>
         <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
           <div className='flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
             <div className='sm:w-[80%] sm:pl-5'>
-              <div className='pt-3 text-gray-700'>Email user nè</div>
+              <div className='pt-3 text-gray-700'>{profile?.email}</div>
             </div>
           </div>
-          {/* <Info /> */}
+          <Info />
           <div>Info</div>
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Địa chỉ</div>
@@ -32,7 +120,7 @@ export default function Profile() {
                 register={register}
                 name='address'
                 placeholder='Địa chỉ'
-                // errorMessage={errors.address?.message}
+                errorMessage={errors.address?.message}
               />
             </div>
           </div>
@@ -40,8 +128,7 @@ export default function Profile() {
             control={control}
             name='date_of_birth'
             render={({ field }) => (
-              // <DateSelect errorMessage={errors.date_of_birth?.message} value={field.value} onChange={field.onChange} />
-              <div>date</div>
+              <DateSelect errorMessage={errors.date_of_birth?.message} value={field.value} onChange={field.onChange} />
             )}
           />
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -97,5 +97,17 @@ export const priceSchema = yup.object({
   })
 })
 
+export const userSchema = yup.object({
+  name: yup.string().max(160, 'Độ dài tối đa là 160 ký tự'),
+  phone: yup.string().max(20, 'Độ dài tối đa là 20 ký tự'),
+  address: yup.string().max(160, 'Độ dài tối đa là 160 ký tự'),
+  avatar: yup.string().max(1000, 'Độ dài tối đa là 1000 ký tự'),
+  date_of_birth: yup.date().max(new Date(), 'Hãy chọn ngày trong quá khứ'),
+  password: schema.fields['password'],
+  new_password: schema.fields['password']
+  // confirm_password: handleConfirmPasswordYup('new_password')
+})
+
 export type SchemaType = yup.InferType<typeof schema>
 export type PriceSchemaType = yup.InferType<typeof priceSchema>
+export type UserType = yup.InferType<typeof userSchema>
